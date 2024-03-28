c4a5fb7b16f557cd54e7e2d95e1a5d0a6d6e9074
axel-zielonka <azielonka@fi.uba.ar>
2024-03-28 13:59:31 -0300

    Cree una funcion para calcular un arreglo de batallas random, ademas cree un archivo de prueba para las salidas esperadas dadas en la consigna

--- TP1/algoritmo.py
+++ TP1/algoritmo.py
@@ -1,5 +1,5 @@
 import time
-
+import random
 
 def leer_archivo(nombre_archivo):
     datos = []
@@ -30,19 +30,29 @@ def minimizar_suma(batallas):
     for i in range(len(batallas_ordenadas)):
         coef_impacto += f[i] * batallas_ordenadas[i][0][1]
 
-    for i in range(len(batallas_ordenadas)):
+    return coef_impacto, batallas_ordenadas
+
+
+def generar_batallas_random(tamanio):
+    batallas = []
+    for i in range(tamanio):
+        t = random.randint(1,1000)
+        b = random.randint(0,1000)
+        batallas.append((t,b))
+    return batallas
+
+def mostrar_orden_batallas(batallas):
+    for i in range(len(batallas)):
         print(
-            "Batalla nro: " + str(batallas_ordenadas[i][1]) + ". Tiempo: " + str(batallas_ordenadas[i][0][0]) + ", peso: " + str(
-                batallas_ordenadas[i][0][1]))
-
-    return coef_impacto
-
+            "Batalla nro: " + str(batallas[i][1]) + ". Tiempo: " + str(batallas[i][0][0]) + ", peso: " + str(
+                batallas[i][0][1]))
 
 if __name__ == "__main__":
     inicio = time.time()
-    coef = minimizar_suma(leer_archivo("archivos/100000.txt"))
+    resultado = minimizar_suma(leer_archivo("archivos/10.txt"))
+    mostrar_orden_batallas(resultado[1])
     print("COEFICIENTE: ")
-    print(coef)
+    print(resultado[0])
     fin = time.time()
     total = (fin-inicio)*1000
     print("TIEMPO TOTAL: " + str(total) + " milisegundos")
