61c3bf30374b1db19c8748f5936974ed28e06486
axel-zielonka <azielonka@fi.uba.ar>
2024-03-29 00:21:26 -0300

    Cree archivos de ejemplo para n = 500, n = 50000, n = 500000, n = 1000000 y n = 10000000. Ademas mediciones.txt muestra el tiempo tardado en realizar el programa para cada uno de los archivos

--- TP1/mediciones.py
+++ TP1/mediciones.py
@@ -5,30 +5,33 @@ import matplotlib.pyplot as plt
 RUTA_10 = "archivos/10.txt"
 RUTA_50 = "archivos/50.txt"
 RUTA_100 = "archivos/100.txt"
+RUTA_500 = "archivos/500.txt"
 RUTA_1000 = "archivos/1000.txt"
 RUTA_5000 = "archivos/5000.txt"
 RUTA_10000 = "archivos/10000.txt"
+RUTA_50000 = "archivos/50000.txt"
 RUTA_100000 = "archivos/100000.txt"
+RUTA_500000 = "archivos/500000.txt"
+RUTA_1000000 = "archivos/1000000.txt"
+RUTA_10000000 = "archivos/10000000.txt"
 
 tiempos = []
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_10))[2])
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_50))[2])
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_100))[2])
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.generar_batallas_random(500))[2])
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_1000))[2])
-#tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_5000))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_10))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_50))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_100))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_500))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_1000))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_5000))[2])
 tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_10000))[2])
-tiempos.append(algoritmo.minimizar_suma(algoritmo.generar_batallas_random(50000))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_50000))[2])
 tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_100000))[2])
-tiempos.append(algoritmo.minimizar_suma(algoritmo.generar_batallas_random(500000))[2])
-tiempos.append(algoritmo.minimizar_suma(algoritmo.generar_batallas_random(1000000))[2])
-tiempos.append(algoritmo.minimizar_suma(algoritmo.generar_batallas_random(10000000))[2])
-valores = [10000,50000,100000,500000,1000000,10000000]
-#valores = [10,50,100,500,1000]#,5000,10000,50000,100000,500000,1000000]
-tabla = []
-tabla.append(valores)
-tabla.append(tiempos)
-print(tiempos)
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_500000))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_1000000))[2])
+tiempos.append(algoritmo.minimizar_suma(algoritmo.leer_archivo(RUTA_10000000))[2])
+valores = [10,50,100,500,1000,5000,10000,50000,100000,500000,1000000,10000000]
 
-plt.plot(tabla[0], tabla[1])
-plt.show()
+with open("archivos/mediciones.txt", "w") as archivo:
+    archivo.write("valores | tiempo tardado\n")
+    for i in range(len(valores)):
+        archivo.write(str(valores[i]) + "," + str(tiempos[i]) + "\n")
+
