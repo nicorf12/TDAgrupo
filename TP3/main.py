27ede6c010b3a11dbf23d93d067d004a70a0ef6e
axel-zielonka <azielonka@fi.uba.ar>
2024-06-09 15:56:53 -0300

    funcion para leer los archivos

---
+++ TP3/main.py
@@ -0,0 +1,21 @@
+import solucion_backtracking
+def main(archivo):
+    maestros = []
+    habilidades = []
+    with open(archivo, "r") as archivo:
+        grupos = int(archivo.readline().strip())
+        maestro, habilidad = map(int, archivo.readline().strip().split(','))
+        maestros.append(maestro)
+        habilidades.append(habilidad)
+    #backtracking:
+    coef, mejor_asignacion = solucion_backtracking.backtrack(maestros, [[] for _ in range(grupos)], [0]*grupos, 0)
+
+    for i, grupo in enumerate(mejor_asignacion, 1):
+        print(f"Grupo {i}: {', '.join(grupo)}")
+    print("Coeficiente:", coef)
+
+    #PL:
+
+
+if __name__ == "__main__":
+    main("archivos/6_3.txt")
