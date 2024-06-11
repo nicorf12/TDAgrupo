7ebd05e785775006108fd969632716558a39016b
axel-zielonka <azielonka@fi.uba.ar>
2024-06-11 19:40:39 -0300

    modifique cosas de las podas

--- TP3/solucion_backtracking.py
+++ TP3/solucion_backtracking.py
@@ -1,3 +1,8 @@
+def grupos_vacios(grupos):
+    vacios = 0
+    for grupo in grupos:
+        if len(grupo) == 0: vacios += 1
+    return vacios
 def _backtrack(maestros_agua, maestros, grupos, fuerzas, fuerza_actual, mejor_coef, mejor_asignacion, k):
     if len(fuerzas) > 0:
         suma_cuadrados = sum(fuerza**2 for fuerza in fuerzas)
@@ -7,12 +12,15 @@ def _backtrack(maestros_agua, maestros, grupos, fuerzas, fuerza_actual, mejor_co
             mejor_asignacion = [grupo.copy() for grupo in grupos]
         return mejor_coef, mejor_asignacion
 
+    #si la suma de los cuadrados ya me da mayor al mejor coeficiente conocido, no tiene sentido seguir por esa rama
     if mejor_coef is not None and suma_cuadrados >= mejor_coef:
-        return mejor_coef, mejor_asignacion
+         return mejor_coef, mejor_asignacion
 
     maestro = maestros.pop()
     for i in range(k):
-        if mejor_coef is None or fuerzas[i] + maestros_agua[maestro] < mejor_coef:
+        if len(maestros) < grupos_vacios(grupos):
+            mejor_coef, mejor_asignacion = _backtrack(maestros_agua, maestros, grupos, fuerzas, fuerza_actual + maestros_agua[maestro], mejor_coef, mejor_asignacion, k)
+        else:
             grupos[i].append(maestro)
             fuerzas[i] += maestros_agua[maestro]
             mejor_coef, mejor_asignacion = _backtrack(maestros_agua, maestros, grupos, fuerzas, fuerza_actual + maestros_agua[maestro], mejor_coef, mejor_asignacion, k)
@@ -29,9 +37,12 @@ def backtrack(maestros, grupos, fuerzas, fuerza_actual, k):
     maestros_nombres = list(maestros.keys())
     if len(maestros) == k:
         mejor_coef = 0
+        grupo_actual = 0
         for maestro in maestros:
             mejor_coef += (maestros[maestro]**2)
-            mejor_asignacion.append(maestro)
+            grupos[grupo_actual].append(maestro)
+            grupo_actual += 1
+            mejor_asignacion = [grupo.copy() for grupo in grupos]
     else:
         mejor_coef, mejor_asignacion = _backtrack(maestros, maestros_nombres, grupos, fuerzas, fuerza_actual, mejor_coef, mejor_asignacion, k)
     return mejor_coef, mejor_asignacion
